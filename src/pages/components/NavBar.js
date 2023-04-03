681d64230c07e1402a6737a78800bc0942db24b2
dominicmena <mena.dominic@gmail.com>
2023-04-03 13:16:05 -0500

    updated motion to stay underlined if in current path

--- src/pages/components/NavBar.js
+++ src/pages/components/NavBar.js
@@ -10,7 +10,7 @@ const CustomLink =({href, title, className=''}) => {
         <Link href={href} className={`${className} relative group`}>
             {title}
 
-            <span className='h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300'>&nbsp;</span>
+            <span className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}>&nbsp;</span>
         </Link>
     )
 }
