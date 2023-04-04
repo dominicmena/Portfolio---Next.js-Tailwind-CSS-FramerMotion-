4a8a00acb445648f4fe461a26c191c60bea7c3ce
dominicmena <mena.dominic@gmail.com>
2023-04-04 16:10:44 -0500

    test

--- src/pages/components/Experience.js
+++ src/pages/components/Experience.js
@@ -1,16 +1,18 @@
 import React from "react";
 
+import { useScroll } from "framer-motion/dist";
+
 const Details = ({ position, company, companyLink, time, address, work }) => {
   return (
-    <li>
+    <li className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
       <div>
-        <h3>
-          {position}&nbsp;<a href={companyLink}>@ {company}</a>{" "}
+        <h3 className="capitalize font-bold text-2xl">
+          {position}&nbsp;<a href={companyLink} target='_blank' className="text-primary capitalize">@ {company}</a>{" "}
         </h3>
-        <span>
+        <span className="capitalize font-medium text-dark/75">
           {time} || {address}
         </span>
-        <p>
+        <p className="font-medium w-full">
             {work}
         </p>
       </div>
@@ -25,8 +27,34 @@ const Experience = () => {
         Experience
       </h2>
       <div className="w-[75%] mx-auto relative">
-        <ul>
+<div className="absolute left-8 top-0 w-[4px] h-full bg-dark origin-top"/>
+
+        <ul className="w-full flex flex-col items-start justify-between ml-4">
           <Details
+          position='Senior Strategy & Operations Associate' company='DoorDash'
+          companyLink='https://www.doordash.com/' time='Aug 2021 - Dec 2022' address='Remote'
+        work='Responsibilities included extracting specific, large data samples related to sales, reconciliations, and consumer reports using SQL queries in MODE. Analyzing the data and providing insights to various stakeholders was also a crucial part of the job. Cross-functional collaboration with account owners ensured a seamless onboarding experience for enterprise merchants. Additionally, commission audits, process gaps, and document improvements were built and scaled by leading projects. To foster merchant relationships and create effective sales strategies, attendance at QBRs with post-sales team members was necessary.'
+          
+          />
+                    <Details
+          position='Senior Strategy & Operations Associate' company='DoorDash'
+          companyLink='https://www.doordash.com/' time='Aug 2021 - Dec 2022' address='Remote'
+        work='Responsibilities included extracting specific, large data samples related to sales, reconciliations, and consumer reports using SQL queries in MODE. Analyzing the data and providing insights to various stakeholders was also a crucial part of the job. Cross-functional collaboration with account owners ensured a seamless onboarding experience for enterprise merchants. Additionally, commission audits, process gaps, and document improvements were built and scaled by leading projects. To foster merchant relationships and create effective sales strategies, attendance at QBRs with post-sales team members was necessary.'
+          
+          />
+                    <Details
+          position='Senior Strategy & Operations Associate' company='DoorDash'
+          companyLink='https://www.doordash.com/' time='Aug 2021 - Dec 2022' address='Remote'
+        work='Responsibilities included extracting specific, large data samples related to sales, reconciliations, and consumer reports using SQL queries in MODE. Analyzing the data and providing insights to various stakeholders was also a crucial part of the job. Cross-functional collaboration with account owners ensured a seamless onboarding experience for enterprise merchants. Additionally, commission audits, process gaps, and document improvements were built and scaled by leading projects. To foster merchant relationships and create effective sales strategies, attendance at QBRs with post-sales team members was necessary.'
+          
+          />
+                    <Details
+          position='Senior Strategy & Operations Associate' company='DoorDash'
+          companyLink='https://www.doordash.com/' time='Aug 2021 - Dec 2022' address='Remote'
+        work='Responsibilities included extracting specific, large data samples related to sales, reconciliations, and consumer reports using SQL queries in MODE. Analyzing the data and providing insights to various stakeholders was also a crucial part of the job. Cross-functional collaboration with account owners ensured a seamless onboarding experience for enterprise merchants. Additionally, commission audits, process gaps, and document improvements were built and scaled by leading projects. To foster merchant relationships and create effective sales strategies, attendance at QBRs with post-sales team members was necessary.'
+          
+          />
+                    <Details
           position='Senior Strategy & Operations Associate' company='DoorDash'
           companyLink='https://www.doordash.com/' time='Aug 2021 - Dec 2022' address='Remote'
         work='Responsibilities included extracting specific, large data samples related to sales, reconciliations, and consumer reports using SQL queries in MODE. Analyzing the data and providing insights to various stakeholders was also a crucial part of the job. Cross-functional collaboration with account owners ensured a seamless onboarding experience for enterprise merchants. Additionally, commission audits, process gaps, and document improvements were built and scaled by leading projects. To foster merchant relationships and create effective sales strategies, attendance at QBRs with post-sales team members was necessary.'
