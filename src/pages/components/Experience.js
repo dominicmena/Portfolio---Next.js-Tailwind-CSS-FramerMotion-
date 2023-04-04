1ac7dccee7dcb2cbf5260280d984250a9b9790db
dominicmena <mena.dominic@gmail.com>
2023-04-04 18:23:20 -0500

    circles are now filling in correct direction and rotated to be on top of the line, adjusted left-margin for line from 8 to 9

--- src/pages/components/Experience.js
+++ src/pages/components/Experience.js
@@ -47,7 +47,7 @@ const Experience = () => {
 
         <motion.div
           style={{scaleY: scrollYProgress}}
-          className="absolute left-8 top-0 w-[4px] h-full bg-dark origin-top"
+          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top"
         />
 
         <ul className="w-full flex flex-col items-start justify-between ml-4">
