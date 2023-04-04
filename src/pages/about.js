390ddee5d8bff4a9a5b2951fba01a7b416e80f02
dominicmena <mena.dominic@gmail.com>
2023-04-04 01:08:38 -0500

    oriented pic and bio in about me and added skills component with background image in tailwind config file

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import AnimatedText from "./components/AnimatedText";
 import Layout from "./components/Layout";
 import profilePic from '../../public/images/profile/IMG_3565.jpg'
 import Image from "next/image";
+import Skills from './components/Skills'
 
 const { default: Head } = require("next/head");
 
@@ -16,14 +17,14 @@ const About = () => {
         <Layout className="pt-16">
           <AnimatedText
             text="Always learning, always creating, always improving."
-            className="!mb-20 !text-lg text-center"
+            className="!mb-2 !text-lg text-left"
           />
-          <div className="grid w-full grid-cols-8 gap-16">
+          <div className="grid w-full grid-cols-6 gap-16 items-center">
             <div className="col-span-3 flex flex-col items-start justify-start">
               <h2 className="mb-1 mt-4 text-lg font-bold uppercase text-dark/75">
                 Biography:
               </h2>
-              <p className="my-4 font-medium mr-0">
+              <p className="my-3 font-medium flex flex-wrap mr-20">
                 Dominic Mena was born and raised in El, Paso TX - A lifelong
                 learner, he recieved his B.A. in Philosophy from Arizona State
                 and went on to work for DoorDash in it's start-up days - scaling
@@ -32,10 +33,12 @@ const About = () => {
                 are favorite pastimes.
               </p>
             </div>
-            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8">
+            <div className="col-span-2 relative h-max rounded-[2rem] border-2 border-solid border-dark bg-light p-9">
+                <div className="absolute top-2 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark"/>
             <Image src={profilePic} alt='Dominic' className='w-full h-auto rounded-2xl'/>
             </div>
           </div>
+          <Skills/>
         </Layout>
       </main>
     </>
---
+++ src/pages/components/Skills.js
@@ -0,0 +1,12 @@
+const Skills = () => {
+    return ( 
+        <>
+        <h2 className="font-bold text-8xl mt-64 w-full text-center">Skills</h2>
+        <div className="w-full h-screen relative flex item-center justify-center rounded-full bg-circularLight">
+
+        </div>
+        </>
+     );
+}
+ 
+export default Skills;
