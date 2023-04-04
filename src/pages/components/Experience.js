4db8ebc443c03e803cce19fbeb1287657ad4a234
dominicmena <mena.dominic@gmail.com>
2023-04-04 17:17:06 -0500

    added li icons

--- src/pages/components/Experience.js
+++ src/pages/components/Experience.js
@@ -3,10 +3,12 @@ import React from "react";
 import { useScroll } from "framer-motion";
 import { useRef } from "react";
 import { motion } from "framer-motion";
+import LiIcon from "./Liicon";
 
 const Details = ({ position, company, companyLink, time, address, work }) => {
   return (
     <li className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
+        <LiIcon/>
       <div>
         <h3 className="capitalize font-bold text-2xl">
           {position}&nbsp;
---
+++ src/pages/components/Liicon.js
@@ -0,0 +1,13 @@
+const LiIcon = () => {
+    return ( 
+        <figure className="absolute left-0 stroke-dark">
+            <svg width='75' height='75' viewBox='0 0 100 100'>
+                <circle cx='75' cy='50' r='20'/>
+                <circle cx='75' cy='50' r='20'/>
+                <circle cx='75' cy='50' r='20'/>
+            </svg>
+        </figure>
+     );
+}
+ 
+export default LiIcon;
