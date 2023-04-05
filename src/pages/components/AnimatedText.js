d54aa3eb1d6125960bcddf78159111e138afdfbb
dominicmena <mena.dominic@gmail.com>
2023-04-05 00:02:07 -0500

    finished home page theme css and now about to import the white versions of my icons so they also switch based on theme selection

--- src/pages/components/AnimatedText.js
+++ src/pages/components/AnimatedText.js
@@ -30,12 +30,13 @@ const singleWord = {
 
 const AnimatedText = ({ text, className = "" }) => {
   return (
-    <div className="w-full mx-auto py-2 flex items-center justify-center overflow-hidden">
+    <div className="w-full mx-auto py-2 flex items-center justify-center overflow-hidden dark:text-light">
       <motion.h1
-        className={`inline-block w-full text-dark font-bold capitalize text-8xl${className}`}
+        className={`inline-block w-full text-dark font-bold capitalize text-8xl${className} dark:text-light`}
         variants={quote}
         initial='initial'
         animate='animate'
+
       >
         {text.split("").map((word, index) => (
           <motion.span key={word + "-" + index} className="inline-block"
