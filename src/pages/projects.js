40dff00dd0d789957d5b61b66b5ebe7999cc81a3
dominicmena <mena.dominic@gmail.com>
2023-04-04 22:18:04 -0500

    added border and general styling to the featured project component

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -10,21 +10,27 @@ import project1 from '../../public/images/projects/crypto-screener-cover-image.j
 const FeaturedProject = ({type, title, summary, img, link, github}) => {
 
     return(
-<article>
-    <Link href={link} target='_blank'>
+<article className="w-full flex items-center justify-between
+rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12
+">
+    <Link href={link} target='_blank'
+    className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+    >
     <Image src={img} alt={title} className='w-full h-auto'/>
     </Link>
 
-    <div>
-        <span>{type}</span>
-        <Link href={link} target='_blank'>
-        <h2>{title}</h2>
+    <div className="w-1/2 flex flex-col items-start justify-between pl-6">
+        <span className="text-primary font-medium text-xl">{type}</span>
+        <Link href={link} target='_blank' className="hover:underline underline-offset-2">
+        <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
 
         </Link>
-        <p>{summary}</p>
-        <div>
-            <Link href={github} target='_blank'> <GitIcon/> </Link>
-            <Link href={link} target='_blank'> Visit Project </Link>
+        <p className="my-2 font-medium text-dark">{summary}</p>
+        <div className="mt-2 flex items-center">
+            <Link href={github} target='_blank' className="w-10"> <GitIcon/> </Link>
+            <Link href={link} target='_blank'
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold"
+            > Visit Project </Link>
         </div>
     </div>
     </article>
