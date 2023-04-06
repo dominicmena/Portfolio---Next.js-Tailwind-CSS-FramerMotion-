c94dc152402888269cae3d7b95341e723c9f31f1
dominicmena <mena.dominic@gmail.com>
2023-04-06 01:17:21 -0500

    updated sizing of text for education

--- src/pages/components/Education.js
+++ src/pages/components/Education.js
@@ -21,7 +21,7 @@ const Details = ({ type, time, place, info }) => {
         <span className="capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm">
           {time} || {place}
         </span>
-        <p className="font-medium w-full md:text-sm">{info}</p>
+        <p className="font-medium w-full md:text-sm xs:text-[17px]">{info}</p>
       </motion.div>
     </li>
   );
