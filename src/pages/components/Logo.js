a5c689fd77c855da1c5d4ed63617f1a1562855f6
dominicmena <mena.dominic@gmail.com>
2023-04-06 02:46:08 -0500

    added pt-1 to logo and centered initials

--- src/pages/components/Logo.js
+++ src/pages/components/Logo.js
@@ -7,7 +7,7 @@ const MotionLink = motion(Link)
 const Logo = () => {
     return ( 
         < div className="flex items-center justify-center mt-3">
-            <MotionLink href='/' className="w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold border border-solid border-transparent dark:border-light"
+            <MotionLink href='/' className="w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold border border-solid border-transparent dark:border-light pt-1"
             whileHover={{
                 backgroundColor: ["#2C2F33", "rgba(204, 51, 63, 1)", "rgba(0, 168, 243, 1)", "rgba(0, 196, 102, 1)", "rgba(255, 196, 0, 1)", "#2C2F33"],
                 transition:{duration:1, repeat: Infinity}
