22602f521d8970c2568d079886ba728caa122adb
dominicmena <mena.dominic@gmail.com>
2023-04-10 18:15:36 -0500

    adjusted Project page animatedtext for mobile

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -136,11 +136,11 @@ const projects = () => {
         <Layout className="pt-16">
           <AnimatedText
             text={`Code today.`}
-            className="!mb-2 !text-4xl text-center lg:!text-3xl sm:mb-8 sm:!text-[20px] xs:!text-[16px]"
+            className="!mb-2 !text-4xl text-center lg:!text-3xl sm:mb-8 sm:!text-[20px] xs:!text-[15px]"
           />
                     <AnimatedText
             text={`innovate  tomorrow.`}
-            className="!mb-2 !text-4xl text-center lg:!text-2xl sm:mb-8 sm:!text-[20px] xs:!text-[15px]"
+            className="!mb-2 !text-4xl text-center lg:!text-2xl sm:mb-8 sm:!text-[20px] xs:!text-[14px]"
           />
 
           <div className="mt-9 grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0 sm:mt-2">
