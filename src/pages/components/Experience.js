96eeba098e5faf3670ae7698f8b87729e111b772
dominicmena <mena.dominic@gmail.com>
2023-08-17 08:43:42 -0500

    updated syntax in details

--- src/pages/components/Experience.js
+++ src/pages/components/Experience.js
@@ -60,7 +60,7 @@ const Experience = () => {
         <Details
             position="Software Engineer Intern"
             company="i4Software"
-            companyLink="https://www.i4software.com/"
+            companyLink="https://www.i4software.com"
             time="July 2023 - Aug 2023"
             address="Remote"
             work="Fully immersed in the entire Software Development Life Cycle (SDLC), experiencing each stage firsthand. From gathering requirements and creating designs to coding, testing, and deploying the apps, I was involved in every step of the process. Collaborating with experienced product managers and software engineers allowed me to continuously learn industry best practices and expand my programming skills. Contributing to real-world projects and witnessing the impact of our work was truly rewarding.
