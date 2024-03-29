81265a427f742cb809984bab1f4a8d4a36136c2c
dominicmena <mena.dominic@gmail.com>
2024-03-29 15:36:41 -0500

    updated resume in link

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,7 +72,7 @@ export default function Home({textContent}) {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="/Resume-Dominic-Mena-4624.pdf"
+                  href="/DominicMenaResume2023.pdf"
                   target={"_blank"}
                   className="flex items-center text-light p-2.5 px-6
           rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
